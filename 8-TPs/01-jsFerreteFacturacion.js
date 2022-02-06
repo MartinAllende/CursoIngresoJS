3faba4307e953e46f94a9e29d5bffd43344bba90
unknown <martin.allende.2002@gmail.com>
2022-02-06 17:35:57 -0300

    tp1 y ejercicios echos

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -4,14 +4,52 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
-	
+{  
+    let numero1;
+    let numero2;
+    let numero3;
+    let resultado;
+
+    numero1 = document.getElementById("txtIdPrecioUno").value;
+	numero2 = document.getElementById("txtIdPrecioDos").value;
+    numero3 = document.getElementById("txtIdPrecioTres").value;
+    resultado = parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
+    alert(resultado.toFixed(3));
+
 }
 function Promedio () 
 {
-	
+    let numero1;
+    let numero2;
+    let numero3;
+    let precioBruto;
+    let resultado;
+
+    numero1 = document.getElementById("txtIdPrecioUno").value;
+	numero2 = document.getElementById("txtIdPrecioDos").value;
+    numero3 = document.getElementById("txtIdPrecioTres").value;
+    precioBruto = parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
+    resultado = parseInt(precioBruto) / 3;
+    alert(resultado.toFixed(3));
+   
 }
 function PrecioFinal () 
 {
-	
+    let numero1;
+    let numero2;
+    let numero3;
+    let precioBruto;
+    let resultado;
+
+    numero1 = document.getElementById("txtIdPrecioUno").value;
+	numero2 = document.getElementById("txtIdPrecioDos").value;
+    numero3 = document.getElementById("txtIdPrecioTres").value;
+    precioBruto = parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
+    resultado = parseInt(precioBruto) * 1.21;
+    alert(resultado.toFixed(3));
+
 }
+
+/* "txtIdPrecioUno"
+   "txtIdPrecioDos"
+   "txtIdPrecioTres" */
